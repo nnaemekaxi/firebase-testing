4503715842ccc7c0f0d7fa9e4c8e279322b37e35
Nnaemeka Joseph <elomnnaemekaj@gmail.com>
2023-04-19 06:11:42 +0100

    media screens

--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -7,16 +7,19 @@ const Test = () => {
 	const [subject, setSubject] = useState("");
 	const [start, setStart] = useState(true);
 	const [allQuestions, setAllQuestions] = useState(null)
-	const [choice, setChoice] = useState("");
-	const [score, setScore] = useState(0);
+	const [choices, setChoices] = useState(null);
 	const [count, setCount] = useState(0);
 	const [finalScore, setFinalScore] = useState(0);
 	const [publish, setPublish] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [response, setResponse] = useState(false);
 	
 	async function handleTest(e){
 		e.preventDefault();
 
-		const mysubject = subject		
+		const mysubject = subject
+		setStart(false)	
+		setIsLoading("Please Wait...");			
 		try {
 			const questions = await axios.get(`https://questions.aloc.com.ng/api/v2/m?subject=${mysubject}`,
 			{
@@ -24,44 +27,72 @@ const Test = () => {
 				"AccessToken": "QB-91e2cfa590f5e7fcd4c8"
 				}
 		})
-			setAllQuestions(questions.data.data)
-			setStart(false)
+		setAllQuestions(questions.data.data);					
 			setTimeout(() => {
-					submitTest();
+				handleSubmit();;
 				}, 1800000)
-
 		} catch (error) {
 			console.log(error.message);
+			setStart(true);
+			setResponse("Network Error! Try Again");
 		}
+		setIsLoading(false)
 	}
 
-	const submitTest = () => {
-		setFinalScore(Number(score));
+	const handleSubmit = () => {
+		let choices = JSON.parse(localStorage.getItem("choices"))
+		let values = Object.values(choices);
+		let score = 0;
+		for (let index = 0; index < values.length; index++) {
+			if (values[index] === true){
+				score +=1;
+			};
+				
+		}
+		setFinalScore(score);
 		setPublish(true);
-		setCount(0);
 		setAllQuestions(null);
 	}
+	const handleChange = (e) => {
+		e.preventDefault();
+		setChoices((prev) => {
+			let data = {...prev, [e.target.name]: e.target.value === e.target.title}
+			console.log(data)
+			localStorage.setItem("choices", JSON.stringify(data));
+			return data;
+	})
+	setChoice((prev) => {
+		let data = {...prev, [e.target.name]: e.target.value}
+		console.log(data)
+		localStorage.setItem("choice", JSON.stringify(data));
+		return data;
+})
+}
+
 	const getPreviousQuestion = () => {
+		handleChange;
 		if (count <= 0){
 			setCount(0)
 		} else{
 			setCount(count-1);
 		}
-		if (allQuestions[count].answer === choice && choice !== null){
-					setScore(Number(score) + 1);
-				} 
 	}
 	const getNextQuestion = () => {
+		handleChange;
 		if (count >=39){
 			setCount(39)
-			submitTest()
+			handleSubmit();
 		} else {
 			setCount(count+1);
 		}
-		if (allQuestions[count].answer === choice && choice !== null){
-			setScore(Number(score) + 1);
-			
-		} 		
+	}
+
+	const ResetTest = () =>{
+		setStart(true);
+		localStorage.removeItem("choices")
+		setCount(0);
+		location.reload();
+		setPublish(false);
 	}
 
 	function TestProp() {
@@ -70,27 +101,24 @@ const Test = () => {
 			{ 
 			  allQuestions.map((question, index) => {
 				if (index == count)
-				return  <div key={index}>
+				return  <div key={question.id}>
 				  <p className={styles.Question}>{index + 1}. {question.question}</p>
 				  {
 					question.image &&
 					<img src={question.image} alt="photo" />
 				  }
-				  <p><input type="radio" name="option" id="optiona" value="a"  autoComplete="off" onChange={((e)=> setChoice(e.target.value))} className={styles.SelectRadio}/> <label htmlFor="optiona"  className={styles.RadioLabel}>a. {question.option.a}</label></p>
-				  <p><input type="radio" name="option" id="optionb" value="b"  autoComplete="off" onChange={((e)=> setChoice(e.target.value))} className={styles.SelectRadio}/> <label htmlFor="optionb"  className={styles.RadioLabel}>b. {question.option.b}</label></p>
-				  <p><input type="radio" name="option" id="optionc" value="c"  autoComplete="off" onChange={((e)=> setChoice(e.target.value))} className={styles.SelectRadio}/> <label htmlFor="optionc"  className={styles.RadioLabel}>c. {question.option.c}</label></p>
-				  <p><input type="radio" name="option" id="optiond" value="d"  autoComplete="off" onChange={((e)=> setChoice(e.target.value))} className={styles.SelectRadio}/> <label htmlFor="optiond"  className={styles.RadioLabel}>d. {question.option.d}</label></p>
-				  </div>
-			   
+				  <form>
+				  <p><input type="radio" name={question.id} title={question.answer} id="optiona" onChange={handleChange} value="a" className={styles.SelectRadio}/> <label htmlFor="optiona"  className={styles.RadioLabel}>a. {question.option.a}</label></p>
+				  <p><input type="radio" name={question.id} title={question.answer} id="optionb" onChange={handleChange} value="b" className={styles.SelectRadio}/> <label htmlFor="optionb"  className={styles.RadioLabel}>b. {question.option.b}</label></p>
+				  <p><input type="radio" name={question.id} title={question.answer} id="optionc" onChange={handleChange} value="c" className={styles.SelectRadio}/> <label htmlFor="optionc"  className={styles.RadioLabel}>c. {question.option.c}</label></p>
+				  <p><input type="radio" name={question.id} title={question.answer} id="optiond" onChange={handleChange} value="d" className={styles.SelectRadio}/> <label htmlFor="optiond"  className={styles.RadioLabel}>d. {question.option.d}</label></p>
+				  </form>
+				  </div>			   
 	  })}	  
 	  </div>
 		)
 	  }
-	
-	const ResetTest = () =>{
-		setStart(true);
-		setPublish(false);
-	}
+
 	return <div className={styles.TestMain}>
 		{
 			start &&
@@ -117,13 +145,20 @@ const Test = () => {
 					<option value="history">Histoty</option>
 				</select>
 				<button type="submit">Start</button>
+
+				<p>{response}</p>
 			</form>
+			
+		}
+		{
+			isLoading && 
+			<h2 className={styles.Loading}>{isLoading}</h2>
 		}
 		{
 			allQuestions &&
 
 			<div>
-			<h1 className={styles.Subject}> <h1>{subject}</h1><h1><Timer duration={30 * 60 * 1000}/></h1></h1>
+			<section className={styles.Subject}> <h1>{subject}</h1><h1><Timer duration={30 * 60 * 1000}/></h1></section>
 			<TestProp
 				allQuestions={allQuestions}
 			/>
@@ -145,5 +180,3 @@ const Test = () => {
 };
 
 export default Test;
-
-
